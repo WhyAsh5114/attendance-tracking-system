928fedb9c4d39aaca8f98d79ab24e5d281baff52
WhyAsh5114 <whyash5114@gmail.com>
2024-02-14 12:31:34 +0530

    trying nfc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		if ('NDEFReader' in window) {
+			const reader = new NDEFReader();
+
+			reader.addEventListener('reading', ({ message }) => {
+				for (const record of message.records) {
+					console.log(record.recordType);
+					console.log(record.data);
+				}
+			});
+
+			reader.scan().catch((error) => {
+				console.error('Error reading NFC: ', error);
+			});
+		} else {
+			console.error('Web NFC is not supported.');
+		}
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
