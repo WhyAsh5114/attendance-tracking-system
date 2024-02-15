1ab860d3226ffb92190939d727435ed2c0c716f2
WhyAsh5114 <whyash5114@gmail.com>
2024-02-15 10:59:41 +0530

    Revert "fix data"
    This reverts commit 07b37280322283587be0bfcb1537029215f394dd.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
+	import { onMount } from 'svelte';
+
+	let errorText = '';
+	let data = '';
+	onMount(async () => {
+		const ndef = new NDEFReader();
+
+		async function startScanning() {
+      console.log("scanning");
+			await ndef.scan();
+			ndef.onreading = (event) => {
+				data = JSON.stringify(event);
+			};
+		}
+
+		const nfcPermissionStatus = await navigator.permissions.query({ name: 'nfc' });
+		if (nfcPermissionStatus.state === 'granted') {
+			// NFC access was previously granted, so we can start NFC scanning now.
+			startScanning();
+		} else {
+			// Show a "scan" button.
+			document.querySelector('#scanButton').style.display = 'block';
+			document.querySelector('#scanButton').onclick = (event) => {
+				// Prompt user to allow UA to send and receive info when they tap NFC devices.
+				startScanning();
+			};
+		}
+	});
 </script>
 
 <Button>Click me</Button>
