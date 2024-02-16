22a60c651b77f382ef700a6d788b4fb7b7b1d1c8
Your Name here only <krishnsundaram@gmail.com>
2024-02-16 21:39:41 +0530

    added defaulter list

--- src/routes/teacher/assigned_lectures/[lectureId]/[attendanceSheets]/+page.svelte
+++ src/routes/teacher/assigned_lectures/[lectureId]/[attendanceSheets]/+page.svelte
@@ -1,28 +1,68 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { DateInput } from 'date-picker-svelte';
+	import MyModal from '$lib/MyModal.svelte';
 	export let data;
 
+	let modal: HTMLDialogElement;
 	const lecture = data.lectures.find((l) => l._id === $page.params.lectureId) as WithSID<Lecture>;
+
 	const students = data.students
 		.filter((s) => lecture.students.includes(s._id))
 		.sort((a, b) => a.rollNumber - b.rollNumber);
 
-	let startDate = new Date();
-	let endDate = new Date();
+	let startDate = new Date(lecture.attendanceSheets[0].timestamp);
+	let endDate = new Date(lecture.attendanceSheets[lecture.attendanceSheets.length - 1].timestamp);
+
+	$: attendanceSheets = lecture.attendanceSheets.filter((a) => {
+		return Number(startDate) <= a.timestamp && a.timestamp <= Number(endDate);
+	});
+
+	let defaulterList: WithSID<Student & { isDefaulter: boolean }>[] = [];
+
+	function generateLectureDefaulters() {
+		defaulterList = students
+			.map((student) => {
+				let studentPresence = 0;
+				attendanceSheets.forEach((attendanceSheet) => {
+					if (attendanceSheet.presentStudents.includes(student._id)) {
+						studentPresence++;
+					}
+				});
+				const isDefaulter = studentPresence / attendanceSheets.length < 0.75;
+				return {
+					...student,
+					isDefaulter
+				};
+			})
+			.filter((student) => student.isDefaulter);
+
+		modal.show();
+	}
 </script>
 
 <h2>Teacher</h2>
 <h3>Attendance sheets for {lecture.name}</h3>
 
+<MyModal title="Defaulter List" bind:dialogElement={modal}>
+	<div class="flex flex-col items-center justify-center gap-1">
+		{#each defaulterList as defaulter}
+			<div class="flex w-full justify-between">
+				<span class="font-semibold text-error">{defaulter.name}</span>
+				<span class="font-semibold">{defaulter.rollNumber}</span>
+			</div>
+		{/each}
+	</div>
+</MyModal>
+
 <div class="flex items-center gap-1 text-sm">
 	<span>Start date: </span>
-	<DateInput bind:value={startDate} format="dd-MM-yy" class="mr-auto"/>
+	<DateInput bind:value={startDate} format="dd-MM-yy" class="mr-auto" />
 	<span>End date: </span>
-	<DateInput bind:value={endDate} format="dd-MM-yy" min={startDate}  dynamicPositioning/>
+	<DateInput bind:value={endDate} format="dd-MM-yy" min={startDate} dynamicPositioning />
 </div>
-<div class="overflow-x-auto">
-	<table class="table">
+<div class="flex h-px grow overflow-auto">
+	<table class="table h-fit">
 		<thead>
 			<tr>
 				<th>Roll No</th>
@@ -36,7 +76,7 @@
 					<td>{student.rollNumber}</td>
 					<td>{student.name}</td>
 					<td class="flex flex-wrap">
-						{#each lecture.attendanceSheets as attendanceSheet}
+						{#each attendanceSheets as attendanceSheet}
 							{#if attendanceSheet.presentStudents.includes(student._id)}
 								<span class="text-accent">P</span>
 							{:else}
@@ -49,6 +89,10 @@
 		</tbody>
 	</table>
 </div>
+
+<button class="btn btn-primary btn-block mt-2" on:click={generateLectureDefaulters}
+	>Generate defaulter sheet</button
+>
 
 <style>
 	:root {
