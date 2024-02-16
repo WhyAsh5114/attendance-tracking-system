51ffe6ec04be979fb86a89aad1c87ffddda1e130
WhyAsh5114 <whyash5114@gmail.com>
2024-02-16 20:38:45 +0530

    added shadcn

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+	darkMode: ['class'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	safelist: ['dark'],
+	plugins: [require('daisyui')]
+};
+
+export default config;
