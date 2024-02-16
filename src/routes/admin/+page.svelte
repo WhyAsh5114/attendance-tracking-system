766b23e30bbef53167408ded89ee24dc67da3204
WhyAsh5114 <whyash5114@gmail.com>
2024-02-16 14:39:47 +0530

    basic admin page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,7 @@
+<h2>Admin</h2>
+
+<ul class="menu w-56 rounded-sm bg-primary text-black">
+	<li><a href="/admin/students">Students</a></li>
+	<li><a href="/admin/teachers">Teachers</a></li>
+	<li><a href="/admin/classes">Classes</a></li>
+</ul>
