7eef7c4c56a16a63aef6d9ce7b591358348c07f3
WhyAsh5114 <whyash5114@gmail.com>
2024-02-17 16:48:09 +0530

    added countdown

--- src/routes/student/[lectureId]/mark_attendance/+page.svelte
+++ src/routes/student/[lectureId]/mark_attendance/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let data;
 	const isMarkingAttendance = data.lecture.isMarkingAttendance as isMarkingAttendanceObject;
-	$: durationLeft = isMarkingAttendance.startTimestamp - Number(new Date());
+	const durationLeft = isMarkingAttendance.startTimestamp - Number(new Date());
 </script>
 
 <h2>Student</h2>
@@ -9,4 +9,8 @@
 
 <span class="m-auto text-5xl font-bold">Go offline</span>
 
-<button class="btn btn-primary">Starting attendance in: {durationLeft}</button>
+<button class="btn btn-primary">
+	Starting attendance in: <span class="countdown">
+		<span style="--value:{(durationLeft / 1000).toFixed(0)};"></span>
+	</span>
+</button>
