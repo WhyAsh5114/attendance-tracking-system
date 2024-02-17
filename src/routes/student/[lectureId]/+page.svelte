3f25b8e85b1f43728729bc3052378124bd0a7f97
WhyAsh5114 <whyash5114@gmail.com>
2024-02-17 15:47:55 +0530

    added lectureID page

---
+++ src/routes/student/[lectureId]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<h2>Student</h2>
+<h3>Lecture {data.lecture.name}</h3>
+
+{#if data.lecture.isMarkingAttendance !== null}
+	<a class="btn btn-primary" href="/student/{data.lecture._id}/mark_attendance">Mark attendance</a>
+{/if}
