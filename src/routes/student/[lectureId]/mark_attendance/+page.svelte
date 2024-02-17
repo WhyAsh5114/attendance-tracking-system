decdb0b6ee124c6175c4793bccc1a208e172798f
WhyAsh5114 <whyash5114@gmail.com>
2024-02-17 17:09:33 +0530

    kgkjgk

--- src/routes/student/[lectureId]/mark_attendance/+page.svelte
+++ src/routes/student/[lectureId]/mark_attendance/+page.svelte
@@ -1,16 +1,35 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let data;
 	const isMarkingAttendance = data.lecture.isMarkingAttendance as isMarkingAttendanceObject;
-	const durationLeft = isMarkingAttendance.startTimestamp - Number(new Date());
+	let durationLeft = Math.round((isMarkingAttendance.startTimestamp - Number(new Date())) / 100);
+
+	let isOffline = false;
+
+	function reduceDuration() {
+		durationLeft -= 1;
+		isOffline = !navigator.onLine;
+		if (durationLeft > 0) {
+			setTimeout(reduceDuration, 100);
+		} else if (durationLeft === 0) {
+		}
+	}
+	onMount(reduceDuration);
 </script>
 
 <h2>Student</h2>
 <h3>Marking attendance for {data.lecture.name}</h3>
 
-<span class="m-auto text-5xl font-bold">Go offline</span>
+{#if !isOffline}
+	<span class="m-auto text-5xl font-bold text-error">Go offline</span>
+{:else}
+	<div class="flex grow flex-col items-center justify-center">
+		<span class="text-5xl font-bold text-primary">You're offline!</span>
+		<span class="text-lg font-semibold">Attendance begins soon</span>
+	</div>
+{/if}
 
 <button class="btn btn-primary">
-	Starting attendance in: <span class="countdown">
-		<span style="--value:{(durationLeft / 1000).toFixed(0)};"></span>
-	</span>
+	Starting attendance in: {Math.round(durationLeft / 10)}
 </button>
