7f98a653df10158f041abee373eefa4c36e4d19b
WhyAsh5114 <whyash5114@gmail.com>
2024-03-17 21:43:27 +0530

    fixed auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,13 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
-import Google from '@auth/sveltekit/providers/google';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
-import clientPromise from '$lib/mongo/mongodb';
-import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, AUTH_SECRET } from '$env/static/private';
+import clientPromise from '../src/lib/mongo/mongodb';
+import github from '@auth/core/providers/github';
+import google from '@auth/core/providers/google';
 
-export const handle = SvelteKitAuth({
-	basePath: '/auth',
-	providers: [Google({ clientId: AUTH_GOOGLE_ID, clientSecret: AUTH_GOOGLE_SECRET })],
-	// @ts-expect-error: still in beta
+export const { handle } = SvelteKitAuth({
 	adapter: MongoDBAdapter(clientPromise),
-	session: { strategy: 'database' },
+	basePath: '/auth',
+	providers: [google, github],
 	callbacks: {
 		// Attach mongoDB user document ID for easier queries
 		async session({ session, user }) {
@@ -17,6 +15,5 @@ export const handle = SvelteKitAuth({
 			return session;
 		}
 	},
-	secret: AUTH_SECRET,
 	trustHost: true
 });
